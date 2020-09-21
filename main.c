d46d96d66f4712e5927382bc7f759732487328b3
Your Name <you@example.com>
2020-09-21 02:57:28 +0200

    second commit

--- main.c
+++ main.c
@@ -3,17 +3,33 @@
 
 int main()
 {
-    int dollars;
-    printf("Enter the price in Dollar\n");
-    scanf("%d", &dollars);
-    int egp = dollars * 15.75;
-    printf("The price in EGP = %d EGP\n" , egp);
+    float price;
+    int menu;
+    printf("Press 1 to convert from Dollar to EGP \nPress 2 to convert from EGP to Dollar\n");
+    scanf("%d", &menu);
+    if (menu == 1){
+        printf("Enter the price in Dollar\n");
+        scanf("%f", &price);
+        float egp = price * 15.74;
+        printf("The price in EGP = %f EGP \nor = %d EGP\n" , egp,(int)egp);
+    }
+    else if (menu == 2){
+        printf("Enter the price in EGP\n");
+        scanf("%f", &price);
+        float egp = price * 0.064;
+        printf("The price in Dollar = %f $ \nor = %d $\n" , egp,(int)egp);
+    }
+    else{
+        printf("I didn't understand You\n");
+        main();
+    }
+
     main1();
     return 0;
 }
 int main1()
 {
-    printf("Press 1 to restart or 0 to exit\n");
+    printf("Press 1 to restart \nPress 0 to exit\n");
     int re;
     scanf("%d", &re);
     if (re == 1){
@@ -21,9 +37,10 @@ int main1()
 
     else if (re == 0){
         printf("Good bye\n");
+        exit(0);
     }
     else {
-        printf("I don't understand\n");
+        printf("I didn't understand You\n");
         main1();
     }
     return 0;
